23804f3acf50fa9cfcb47939fa7c82b04505a4ec
naganithinkalthireddy <125631996+naganithinkalthireddy@users.noreply.github.com>
2023-02-17 10:41:37 +0530

    Add files via upload

---
+++ one.java
@@ -0,0 +1,73 @@
+1 public class Literals
+public static void main(String[] args)
+AT
+boolean b = true;
+boolean = false;
+boolean d = 0;
+boolean b = 1;
+System.out.println(b);
+System.out.println(c); System.out.println(d);
+12
+System.out.println(e);
+11
+131 public class Literals
+public static void main(String[] args)
+AT
+boolean b = true;
+boolean = false;
+boolean d = 0;
+boolean b = 1;
+System.out.println(b);
+System.out.println(c); System.out.println(d);
+12
+System.out.println(e);
+11
+131 public class Literals
+public static void main(String[] args)
+AT
+boolean b = true;
+boolean = false;
+boolean d = 0;
+boolean b = 1;
+System.out.println(b);
+System.out.println(c); System.out.println(d);
+12
+System.out.println(e);
+11
+131 public class Literals
+public static void main(String[] args)
+AT
+boolean b = true;
+boolean = false;
+boolean d = 0;
+boolean b = 1;
+System.out.println(b);
+System.out.println(c); System.out.println(d);
+12
+System.out.println(e);
+11
+131 public class Literals
+public static void main(String[] args)
+AT
+boolean b = true;
+boolean = false;
+boolean d = 0;
+boolean b = 1;
+System.out.println(b);
+System.out.println(c); System.out.println(d);
+12
+System.out.println(e);
+11
+131 public class Literals
+public static void main(String[] args)
+AT
+boolean b = true;
+boolean = false;
+boolean d = 0;
+boolean b = 1;
+System.out.println(b);
+System.out.println(c); System.out.println(d);
+12
+System.out.println(e);
+11
+13
